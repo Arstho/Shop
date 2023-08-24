13807c5696eb350bbadaf4ae7d6e8a4af3574528
Aslan <aslan86@mail.ru>
2023-08-25 02:38:27 +0300

    сделал модальное окно

--- src/components/FullItem.jsx
+++ src/components/FullItem.jsx
@@ -4,11 +4,13 @@ const FullItem = ({ item, onShowItem, onAdd }) => {
 
    return (
       <div className='full-item'>
-         <img src={'./img/' + item.img} alt="img" onClick={() => onShowItem(item)} />
-         <h2>{item.title}</h2>
-         <p>{item.desc}</p>
-         <b>{item.price}$</b>
-         <div className='add-to-cart' onClick={() => onAdd(item)}>+</div>
+         <div>
+            <img src={'./img/' + item.img} alt="img" onClick={() => onShowItem(item)} />
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <b>{item.price}$</b>
+            <div className='add-to-cart' onClick={() => onAdd(item)}>+</div>
+         </div>
       </div>
    )
 }
